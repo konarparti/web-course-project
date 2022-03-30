70d5904c509e6c45b953c98340c33f9424200c13
Anton <anton.gusevv.01@gmail.com>
2022-03-30 18:31:17 +0300

    add amCharts

--- js/graph.js
+++ js/graph.js
@@ -1,26 +1,39 @@
 
-var viz;
+let chart = am4core.create("graph", am4plugins_forceDirected.ForceDirectedTree);
 
-function draw() {
-    var config = {
-        container_id: "graph",
-        server_url: "bolt://localhost:7687",
-        server_user: "neo4j",
-        server_password: "root",
-        labels: {
-            "Polymer": {
-                "caption": "name"
-            }
-        },
-        relationships: {
-            "INTERACTS": {
-                "thickness": "weight",
-                "caption": false
-            }
-        },
-        initial_cypher: "MATCH (n)-[r]->(m) RETURN *"
-    };
+let series = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries());
 
-    viz = new NeoVis.default(config);
-    viz.render();
-}
+series.colors.list = [
+    am4core.color("#acd")
+];
+
+series.data = [{
+        "name": "SBS",
+        "value": 1,
+        "link": ["PB"]
+    }, {
+        "name": "PS",
+        "value": 1,
+        "link": ["PB"]
+    }, {
+        "name": "PE",
+        "value": 1,
+        "link": ["PB"]
+    },
+    {
+        "name": "PB",
+        "value": 1
+    }];
+// Set up data fields
+series.dataFields.id = "name";
+series.dataFields.value = "value"
+series.dataFields.linkWith = "link";
+// Add labels
+series.nodes.template.label.text = "{name}";
+series.fontSize = 16;
+series.minRadius = 35;
+series.maxRadius = 50;
+series.links.template.strokeWidth = 3;
+series.links.template.strokeOpacity = 1;
+
+series.centerStrength = 0.1;
