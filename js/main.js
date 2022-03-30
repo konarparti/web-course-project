95897489d95bb9c936192c0b6834ec1bd4a154f6
AntonLaptop <forstudinggusev@gmail.com>
2022-03-30 12:22:56 +0300

    fix menu bug and add changing views

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ const makeActive = () => {
     navToggleItem.classList.toggle("active")
 }
 navToggle.addEventListener('click', makeActive);
+
+const blockView = document.getElementById('block-view');
+const inlineView = document.getElementById('inline-view');
+
+const changeOnBlockView = () =>{
+    blockView.style.display = "";
+    inlineView.style.display = "none";
+}
+const changeOnInlineView = () =>{
+    inlineView.style.display = "";
+    blockView.style.display = "none";
+}
